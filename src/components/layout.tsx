3fcea98d163054e4fb2a449258e27fbb8240f4c8
J70istooshort <J70istooshort@gmail.com>
2021-10-14 17:50:05 +0100

    Update layout.tsx

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -39,8 +39,14 @@
        >
          <main>{children}</main>
        </div>
-       
-       <Footer />
+
+       <Footer style={{
+        position: 'fixed',
+        padding: '10px 10px 0px 10px',
+        bottom: '0',
+        width: '100%',
+       }}
+       />
      </>
    )
  }
