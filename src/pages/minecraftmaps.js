26ccccccef5b75cdc5f218ad527b27543697537a
J70istooshort <J70istooshort@gmail.com>
2021-10-14 14:44:11 +0100

    added new page

---
+++ src/pages/minecraftmaps.js
@@ -0,0 +1,16 @@
+import * as React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const MinecraftMaps = () => (
+  <Layout>
+    <Seo title="Minecraft Maps" />
+    <h1>Minecraft Maps</h1>
+
+    <Link to="/">Go back to the homepage</Link>
+  </Layout>
+)
+
+export default MinecraftMaps
